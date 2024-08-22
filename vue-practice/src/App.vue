e9c45592f0fc35b5f59fc8fa758c41eb18be3391
codenamehj <loveim1212@naver.com>
2024-08-23 00:00:52 +0900

    240822  vue.js 프로젝트 투입 일주일 전 ~p.110

--- vue-practice/src/App.vue
+++ vue-practice/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/databindingstyle2">Data Binding</router-link>
   </nav>
   <router-view/>
 </template>
---
+++ vue-practice/src/views/About.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1>About</h1>
+</template>
+<script>
+export default {
+    components: {},
+    data() {
+        return {
+            sampleData: ''
+        };
+    },
+    beforeCreate() { },
+    created() { },
+    beforeMount() { },
+    mounted() { },
+    beforeUpdate() { },
+    updated() { },
+    beforeUnmount() { },
+    unmounted() { },
+    methods: {}
+}
+</script>
